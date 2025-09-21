45627350c2776c1ae733a0333fad04f999de4445
Julieta-8 <48799592@est.ort.edu.ar>
2025-09-21 17:25:09 -0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,11 +14,30 @@ public class HomeController : Controller
     }
   [HttpPost]public IActionResult Comenzar(string username, int dificultad)
 {
+/* Juego juego = HttpContext.Session.GetObject<Juego>("Juego");
 
-    Juego.InicializarJuego(username, dificultad);
-   HttpContext.Session.SetString("User", username.ToString());
-  
+    if (juego == null)
+    {
+  Juego.InicializarJuego(username, dificultad);
+    
 string palabra =  CargarPalabra(dificultad);
+    }
+
+    var jugador = juego.Jugadores.FirstOrDefault(j => j.Nombre == nombre);
+
+    if (jugador != null)
+    {
+     
+        return Json(new
+        {
+            existe = true,
+            nombre = jugador.Nombre,
+            intentos = jugador.CantidadIntentos,
+            palabra = juego.PalabraActual
+        });*/
+  
+
+
    
      ViewBag.palabra = "palabra";
         ViewBag.username = "username";
